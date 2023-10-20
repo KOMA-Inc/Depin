a13b3cb5bb8cb4c02bcfb037c7935f8248525374
Mykola Zhurba <gloomikon@gmail.com>
2023-10-20 14:08:52 +0200

    Add public to helper methods

--- Sources/Depin/Swinject+Extension.swift
+++ Sources/Depin/Swinject+Extension.swift
@@ -2,14 +2,14 @@ import Swinject
 
 infix operator ~>
 
-func ~><Service>(container: Container, type: Service.Type) -> Service {
+public func ~><Service>(container: Container, type: Service.Type) -> Service {
     container.resolve(type)!
 }
 
-func ~><Service>(container: Container, data: (type: Service.Type, name: String?)) -> Service {
+public func ~><Service>(container: Container, data: (type: Service.Type, name: String?)) -> Service {
     container.resolve(data.type, name: data.name)!
 }
 
-func ~><Service>(resolver: Resolver, type: Service.Type) -> Service {
+public func ~><Service>(resolver: Resolver, type: Service.Type) -> Service {
     resolver.resolve(type)!
 }
